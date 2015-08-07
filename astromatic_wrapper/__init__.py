7bd7d8d363b2636049cfacd0cb98dba3692592c7
Fred Moolekamp <fred3public@gmail.com>
2015-08-07 00:37:28 -0400

    Removed example_mod

--- astromatic_wrapper/__init__.py
+++ astromatic_wrapper/__init__.py
@@ -15,7 +15,5 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from example_mod import *
-
-import astromatic_wrapper.api
-import astromatic_wrapper.utils
+    import astromatic_wrapper.api
+    import astromatic_wrapper.utils
